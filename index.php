bdfd4aa7ca9f489f1d2f345313af8e810e16f27c
Angella-Malayika <nangella299@gmail.com>
2026-05-09 22:03:43 +0300

    footer fixed

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-    <?php include '../includes/footer.php'; ?>
+    <?php include 'includes/footer.php'; ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
